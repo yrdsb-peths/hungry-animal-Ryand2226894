bcf303dbd354be01568c4d9fc8699866174a078e
Ryand2226894 <167019566+Ryand2226894@users.noreply.github.com>
2024-05-13 15:37:16 -0400

    made elephant bigger

--- elephant.java
+++ elephant.java
@@ -9,17 +9,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage[] idle = new GreenfootImage 
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     
-    public Elephant()
+public Elephant()
+{
+    for(int i = 0; i < 8; i++)
     {
-        for(int i = 0; i < 8; i++)
-        {
-            
-        }
-        setImage(idle);
+        idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        idle[i].scale(100, 100);
     }
+    setImage(idle[0]); 
+}
+int imageIndex = 0;
+
+public void animateElephant()
+{
+    setImage(idle[imageIndex]);
+    imageIndex = (imageIndex + 1) % idle.length;
+}
+
     public void act()
     {
         // Add your action code here.
@@ -34,6 +43,8 @@ public class Elephant extends Actor
         }
         
         eat();
+        animateElephant();
+        
     }
     
     public void eat()
