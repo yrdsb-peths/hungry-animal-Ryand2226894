4140665e7efba7242ab79ae2cd1c16c449135bc5
Ryand2226894 <167019566+Ryand2226894@users.noreply.github.com>
2024-05-20 11:00:31 -0400

    added a test in the middle of the world saying "hungry elephant"

--- TitleScreen.java
+++ TitleScreen.java
@@ -3,12 +3,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class TitleScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ryan
+ * @version may 2024
  */
 public class TitleScreen extends World
 {
-
+    Label titleLabel = new Label("hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,5 +17,6 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
     }
 }
